1416b54878774480874d916df9078a669aff8953
Rithreaksa <wonderfulreaksa@gmail.com>
2020-12-04 21:06:34 +0100

    Implement search functionality
    - Add more test for user can search for movies
     - Add new movies json file for search function

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,38 +1,63 @@
 class Api::MoviesController < ApplicationController
-  API_KEY = Rails.application.credentials.moviedb[:api_key]  
-  API_URL = "https://api.themoviedb.org/3"
+  API_KEY = Rails.application.credentials.moviedb[:api_key]
+  API_URL = 'https://api.themoviedb.org/3'
 
-  before_action :authenticate_user!, only: [:search]
+  before_action :authenticate_user!, only: %i[search]
 
   def index
-    result = RestClient.get("#{API_URL}/discover/movie", params: 
-      {api_key: API_KEY, language: "en-US", sort_by: "popularity.desc", include_adult: "false", page: "1"})
+    result =
+      RestClient.get(
+        "#{API_URL}/discover/movie",
+        params: {
+          api_key: API_KEY,
+          language: 'en-US',
+          sort_by: 'popularity.desc',
+          include_adult: 'false',
+          page: '1'
+        }
+      )
     body = JSON.parse(result.body)
-    movies = sanitized_movies(body)  
+    movies = sanitized_movies(body)
 
     render json: { movies: movies }, status: 200
   end
 
   def search
-    render json: {}
+    if params[:query] == nil
+      render json: { message: 'You need to fill in the search form' },
+             status: 422
+    else
+      result =
+        RestClient.get(
+          "#{API_URL}/search/movie",
+          params: {
+            api_key: API_KEY,
+            language: 'en-US',
+            query: params[:query],
+            include_adult: 'false',
+            page: '1'
+          }
+        )
+      body = JSON.parse(result.body)
+      movies = sanitized_movies(body)
+      render json: { movies: movies }, status: 200
+    end
   end
 
   private
 
   def sanitized_movies(body)
     sanitized_movies = []
-    array = body["results"]
-    array.each do |movie|
-      sanitized_movies.push format_movie(movie)
-    end
+    array = body['results']
+    array.each { |movie| sanitized_movies.push format_movie(movie) }
     sanitized_movies
   end
 
   def format_movie(movie)
     {
-      title: movie["title"],
-      release_date: movie["release_date"],
-      poster_path: movie["poster_path"]
+      title: movie['title'],
+      release_date: movie['release_date'],
+      poster_path: movie['poster_path']
     }
   end
 end
--- spec/requests/api/user_can_search_for_movie_by_title_spec.rb
+++ spec/requests/api/user_can_search_for_movie_by_title_spec.rb
@@ -5,15 +5,59 @@ RSpec.describe Api::MoviesController, type: :request do
   let!(:authorization_headers) { registered_user.create_new_auth_token }
 
   describe 'Search for a movie by title' do
+    context 'When users provide query params' do
+      before do
+        fixture_file =
+          File.open("#{::Rails.root}/spec/fixtures/movies_search.json").read
+        query = 'christmas'
+        stub_request(
+          :get,
+          "https://api.themoviedb.org/3/search/movie?api_key=#{
+            Rails.application.credentials.moviedb[:api_key]
+          }&language=en-US&query=#{query}&page=1&include_adult=false"
+        ).to_return(status: 200, body: fixture_file, headers: {})
 
-    before do
-      get '/api/movies/search',
-        params: { query: 'Christmas' },
-        headers: authorization_headers 
+        get '/api/movies/search',
+            params: { query: query }, headers: authorization_headers
+      end
+
+      it 'is expected to return a 200 response' do
+        expect(response).to have_http_status 200
+      end
+
+      it 'is expected to return twenty movies' do
+        expect(response_json['movies'].count).to eq 20
+      end
+
+      it 'is expected to have movie title in \'movies\'' do
+        expect(
+          response_json['movies'].first['title']
+        ).to eq 'The Christmas Chronicles: Part Two'
+      end
+
+      it 'is expected to have release date in \'movies\'' do
+        expect(
+          response_json['movies'].second['release_date']
+        ).to eq '2020-11-05'
+      end
+
+      it 'is expected to have movie poster in \'movies\'' do
+        expect(
+          response_json['movies'].third['poster_path']
+        ).to eq '/eu747ko823mktL3ygo7ohdZznP7.jpg'
+      end
     end
-    
-    it {
-      expect(response).to have_http_status 200
-    }
+  end
+
+  context 'When users do not provide query params' do
+    before { get '/api/movies/search', headers: authorization_headers }
+
+    it 'is expected to return a 422 response' do
+      expect(response).to have_http_status 422
+    end
+
+    it 'is expected to return error message' do
+      expect(response_json['message']).to eq 'You need to fill in the search form'
+    end
   end
 end
